5cf4a768cd3d18748754dd9f0e517b1c44498324
David Sickmiller <david@asknice.ly>
2023-03-25 14:46:52 +1300

    Make it a collection and add in the total cost

--- app/ProgrammingTest/DeliveryCostCalculator.php
+++ app/ProgrammingTest/DeliveryCostCalculator.php
@@ -4,22 +4,30 @@ namespace ProgrammingTest;
 
 class DeliveryCostCalculator
 {
-    public static function calculateCost(Item $item): Item
+    /**
+     * @param Item[] $items
+     */
+    public static function calculateCost(array $items): array
     {
-        $longestDimension = max($item->width, $item->length, $item->height);
-        if ($longestDimension < 10) {
-            $item->cost = 3;
-            $item->type = ItemType::Small;
-        } else if ($longestDimension < 50) {
-            $item->cost = 8;
-            $item->type = ItemType::Medium;
-        } else if ($longestDimension < 100) {
-            $item->cost = 15;
-            $item->type = ItemType::Large;
-        } else {
-            $item->cost = 25;
-            $item->type = ItemType::XLarge;
+        $totalCost = 0;
+        foreach ($items as $item) {
+            $longestDimension = max($item->width, $item->length, $item->height);
+            if ($longestDimension < 10) {
+                $item->cost = 3;
+                $item->type = ItemType::Small;
+            } else if ($longestDimension < 50) {
+                $item->cost = 8;
+                $item->type = ItemType::Medium;
+            } else if ($longestDimension < 100) {
+                $item->cost = 15;
+                $item->type = ItemType::Large;
+            } else {
+                $item->cost = 25;
+                $item->type = ItemType::XLarge;
+            }
+            $totalCost += $item->cost;
         }
-        return $item;
+
+        return ['items' => $items, 'totalCost' => $totalCost];
     }
 }
--- tests/DeliveryCostCalculatorTest.php
+++ tests/DeliveryCostCalculatorTest.php
@@ -11,9 +11,11 @@ class DeliveryCostCalculatorTest extends TestCase
 {
     public function testSmallParcel()
     {
-        $item = new Item(5.0, 5.0, 5.0);
-        $returnVal = DeliveryCostCalculator::calculateCost($item);
-        $this->assertSame(3, $returnVal->cost);
-        $this->assertSame(ItemType::Small, $returnVal->type);
+        $items = [new Item(5.0, 5.0, 5.0)];
+        $returnVal = DeliveryCostCalculator::calculateCost($items);
+        $this->assertSame(3, $returnVal['totalCost']);
+        $this->assertCount(1, $returnVal['items']);
+        $this->assertSame(3, $returnVal['items'][0]->cost);
+        $this->assertSame(ItemType::Small, $returnVal['items'][0]->type);
     }
 }
